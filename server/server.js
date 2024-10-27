ebb95b2d830d1bf523387e5af78eee0fdf4e64ec
erfanpv <erfanpv786@gmail.com>
2024-10-27 21:57:13 +0530

    chore: update environment and dependencies

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDb from "./src/config/db.js";
+import authRouter from "./src/routes/authRoutes.js"
+import userRouter from "./src/routes/userRoutes.js";
 
 const app = express();
 dotenv.config();
@@ -14,7 +16,8 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
 app.use("*", (req, res) => {
   res.status(404).json({ success: false, message: "This route is not available." });
 });
