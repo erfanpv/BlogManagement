f5ab2fac9b1a12b337509f910d18a5420d196021
erfanpv <erfanpv786@gmail.com>
2024-10-29 20:15:35 +0530

    chore: update Next.js configuration

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,7 +3,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn-icons-png.flaticon.com", "www.citypng.com", 'currentaffairs.adda247.com', "via.placeholder.com"],
+    domains: [
+      "cdn-icons-png.flaticon.com",
+      "www.citypng.com",
+      "currentaffairs.adda247.com",
+      "via.placeholder.com",
+      "res.cloudinary.com",
+    ],
   },
 };
 
