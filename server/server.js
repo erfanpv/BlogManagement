7b253828d9990787b825d864a68b0afbed304c82
erfanpv <erfanpv786@gmail.com>
2024-10-29 20:02:29 +0530

    chore: update project dependencies, server setup, and gitignore

--- server/server.js
+++ server/server.js
@@ -3,26 +3,31 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDb from "./src/config/db.js";
-import authRouter from "./src/routes/authRoutes.js"
+import authRouter from "./src/routes/authRoutes.js";
 import userRouter from "./src/routes/userRoutes.js";
+import baseRouter from "./src/routes/baseRoutes.js";
+import adminRouter from "./src/routes/adminRoutes.js";
 
 const app = express();
 dotenv.config();
 
 connectDb();
 
-app.use(cors());  
+app.use(cors());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use("/api/auth", authRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/user", userRouter);
+app.use("/api/base", baseRouter);
 app.use("*", (req, res) => {
   res.status(404).json({ success: false, message: "This route is not available." });
 });
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () =>
   console.log(`Server Listening on port http://localhost:${PORT}`)
 );
